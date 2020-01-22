603eaa61c024424d767463c5dc7356a42f7cd2dc
Lessllie Real <rlessllie@yahoo.com>
2020-01-22 07:41:26 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -6,11 +6,3 @@ def square_array(array)
   new_array
 end
 
-def square_array(array)
-  arr = []
-  array.each { |i| arr << i ** 2 }
-  arr
-end
-
-my_arr = [1, 2]
-p square_array(my_arr) #=> [1, 4]
